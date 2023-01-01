3ce336642b100b6b04f8976591e4821cfc9d6280
Anurag <akushwaha@svam.com>
2023-01-01 20:33:48 +0530

    guestcheck

--- payment_success.php
+++ payment_success.php
@@ -7,7 +7,21 @@
                 <p>
                     <!-- <h3 style="margin-top:20px;"><?php echo LANG_VALUE_121; ?></h3> -->
                     <h3 style="margin-top:20px;">Order Placed Successfully...</h3>
-                    <a href="dashboard.php" class="btn btn-success"><?php echo LANG_VALUE_91; ?></a>
+                    <?php
+					if(isset($_SESSION['customer'])) {
+						if($_SESSION['customer']['cust_guest']===1) {
+							?>
+							<p>You will get order confirmation on your email.</p>
+							<?php
+                            header("refresh:3;url= ".BASE_URL."logout.php");
+						}
+                        else{
+                            ?>
+                            <a href="dashboard.php" class="btn btn-success"><?php echo LANG_VALUE_91; ?></a>
+                        <?php
+                        }
+					}
+					?>
                 </p>
             </div>
         </div>
